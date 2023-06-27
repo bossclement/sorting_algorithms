518518523b23cc2d7cdf501e6327a7cae24c01d0
bossclement <bossclement@gmail.com>
2023-06-27 22:04:05 +0200

    New commit

---
+++ 3-quick_sort.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include "sort.h"
+
+int partition(int *array, int low, int high, size_t size);
+void quick_sort(int *array, size_t size);
+void quicksort_recursive(int *array, int low, int high, size_t size);
+
+/**
+ * quick_sort - Sorts an array of integers in ascending
+ *		order using the Quick sort algorithm.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size <= 1)
+		return;
+
+	quicksort_recursive(array, 0, size - 1, size);
+}
+
+/**
+ * quicksort_recursive - Recursive function to perform Quick sort.
+ * @array: The array to be sorted.
+ * @low: The starting index of the partition.
+ * @high: The ending index of the partition.
+ * @size: The size of the array.
+ */
+void quicksort_recursive(int *array, int low, int high, size_t size)
+{
+	int pivot;
+
+	if (low < high)
+	{
+		pivot = partition(array, low, high, size);
+		quicksort_recursive(array, low, pivot - 1, size);
+		quicksort_recursive(array, pivot + 1, high, size);
+	}
+}
+
+/**
+ * partition - Partitions the array and selects a pivot element.
+ * @array: The array to be partitioned.
+ * @low: The starting index of the partition.
+ * @high: The ending index of the partition.
+ * @size: The size of the array.
+ *
+ * Return: The index of the pivot element.
+ */
+int partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j, temp;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+
+	if (array[i + 1] != array[high])
+	{
+		temp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
+
+	return (i + 1);
+}
+
